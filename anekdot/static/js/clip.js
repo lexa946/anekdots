15fa8d219933b0165e9d77ee09c199a196bd7871
Алексей Пожар <a.pozhar@x5.ru>
2024-09-13 11:18:11 +0300

    Поправил баг с клипом ссылки

--- anekdot/static/js/clip.js
+++ anekdot/static/js/clip.js
@@ -5,9 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
     clipButtons.forEach(button => {
         button.addEventListener('click', event => {
             let anekdot_id = button.parentNode.parentNode.parentNode.getAttribute('anekdot_id');
+            let inputClip = button.parentNode.querySelector('input');
             let link = location.protocol + "//"+location.host+"/anekdot/"+anekdot_id;
-            navigator.clipboard.writeText(location.protocol + "//"+location.host+"/anekdot/"+anekdot_id).then()
-            button.setAttribute('title', 'Ссылка скопирована: '+link)
+            input.value = link;
+//            navigator.clipboard.writeText(location.protocol + "//"+location.host+"/anekdot/"+anekdot_id).then()
+            input.select();
+            if(document.execCommand('copy')){
+                button.setAttribute('title', 'Ссылка скопирована: '+link)
+            }else{
+                button.setAttribute('title', 'Не удалось поместить ссылку в буфер: '+link)
+            }
         });
 
     });
